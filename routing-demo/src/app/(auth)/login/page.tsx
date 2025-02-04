d30aee40f229eaa70f9dd593676fec567a9b590b
abh1nav9 <ABHINAVGAUTAM.210111063@gehu.ac.in>
2025-02-04 13:44:38 +0530

    Private Folder

--- routing-demo/src/app/(auth)/login/page.tsx
+++ routing-demo/src/app/(auth)/login/page.tsx
@@ -1,7 +1,15 @@
-export default function Login(){
-    return(
-        <>
+import Hello from "@/app/_components/hello";
+
+export default function Login() {
+  return (
+    <>
+      <div>
         <h1>Login</h1>
-        </>
-    )
+        <div>
+          <Hello />
+        </div>
+      </div>
+    </>
+  );
 }
+    
