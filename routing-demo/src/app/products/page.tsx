4c3af6a810794e1f351f7f19968cd59eec30d5dd
abh1nav9 <ABHINAVGAUTAM.210111063@gehu.ac.in>
2025-02-05 22:17:36 +0530

    Link Components

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function ProductList(){
+    const productId = 1;
     return(
         <>
         <div>
             <h1>Product List</h1>
-            <h2>Product 1</h2>
+            <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2>
             <h2>Product 2</h2>
             <h2>Product 3</h2>
         </div>
